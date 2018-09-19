7b4f8717f14962780c6408e154291577270bbdf5
tugang <tu.gang@navercorp.com>
2018-09-19 14:23:36 +0800

    init

---
+++ src/main/java/com/teachy/coins/CoinsApplication.java
@@ -0,0 +1,12 @@
+package com.teachy.coins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoinsApplication.class, args);
+	}
+}
---
+++ src/main/java/com/teachy/coins/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.teachy.coins;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(CoinsApplication.class);
+	}
+
+}
