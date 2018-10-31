2ca3859467ed441e60ec4363c53cdd7cfee645d7
tugang <tu.gang@navercorp.com>
2018-10-31 15:06:51 +0800

    add swagger test model

---
+++ src/main/java/com/teachy/coins/controller/BaseController.java
@@ -0,0 +1,21 @@
+package com.teachy.coins.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.teachy.coins.mapper.BaseCoinsDAO;
+import com.teachy.coins.mapper.KlineDAO;
+import com.teachy.coins.mapper.WarningDAO;
+
+@Component
+public class BaseController {
+	protected final Logger logger = LoggerFactory.getLogger(this.getClass());
+	@Autowired
+	protected KlineDAO klineDAO;
+	@Autowired
+	protected BaseCoinsDAO baseCoinsDAO;
+	@Autowired
+	protected WarningDAO warningDAO;
+}
