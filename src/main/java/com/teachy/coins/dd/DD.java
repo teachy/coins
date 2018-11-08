2e0385054863d5446ae941dcb75dde9ed40efed4
tugang <306056969@qq.com>
2018-11-08 21:29:33 +0800

    dev

---
+++ src/main/java/com/teachy/coins/dd/DD.java
@@ -0,0 +1,17 @@
+package com.teachy.coins.dd;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class DD {
+    long temQH;
+    int jieguo;
+    String kjTime;
+    String jcTime;
+}
